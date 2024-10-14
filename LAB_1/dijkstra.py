a825325e6ceb559b490d03feb48a5544589aa9b9
NCCMNT <chmielewski2003@gmail.com>
2024-10-14 13:06:07 +0200

    lab 1

---
+++ LAB_1/dijkstra.py
@@ -0,0 +1,36 @@
+from queue import PriorityQueue
+
+INF = float('inf')
+
+def Dijkstra(G, start):
+    n = len(G)
+    distances = [-INF for _ in range(n)]
+    distances[start] = 0
+    visited = [False for _ in range(n)]
+    parent = [None for _ in range(n)]
+    PQ = PriorityQueue()
+    PQ.put((0,start))
+
+    while not PQ.empty():
+        _w, u = PQ.get()
+
+        if visited[u]: continue
+        visited[u] = True
+
+        for v, cost in G[u]:
+            if not visited[v] and distances[v] < cost:
+                parent[v] = u
+                distances[v] = cost
+                PQ.put((-cost,v))
+
+    parent[0] = None
+    return parent, distances
+
+def getSolution(G,start):
+    parent, distances = Dijkstra(G,start)
+    par = parent[1]
+    res = distances[1]
+    while par != 0:
+        res = min(res,distances[par])
+        par = parent[par]
+    return res
