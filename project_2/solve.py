e4fedb0dc9583aa78d6032813d0c3667333a2b9a
NCCMNT <chmielewski2003@gmail.com>
2025-02-08 22:40:00 +0100

    take 2

--- project_2/solve.py
+++ project_2/solve.py
@@ -1,9 +1,12 @@
 from data import runtests
+import sys
+sys.setrecursionlimit(10**6)
 
 class Board:
-    def __init__(self, id : int, pieces_position : list[tuple [str, int, int]]):
+    def __init__(self, id : int, pieces_position : set[tuple [str, int, int]]):
         self.id = id
         self.pieces_positions = pieces_position
+        self.is_winning = None
     
     def __eq__(self, other):
         return self.pieces_positions == other.pieces_positions
@@ -13,7 +16,7 @@ class Board:
 
 def my_solve(N, M, holes, pieces):
 
-    def get_move_set(piece, x, y):
+    def get_move_set(piece, x, y, other_pices):
 
         move_set = []
 
@@ -23,14 +26,14 @@ def my_solve(N, M, holes, pieces):
             case "k":
                 moves = [(x, y + 1), (x + 1, y + 1), (x + 1, y), (x + 1, y - 1), (x, y - 1), (x - 1, y - 1), (x - 1, y), (x - 1, y + 1)]
                 for x, y in moves:
-                    if (0 < x <= N) and (0 < y <= M) and (x,y) not in holes:
+                    if (0 < x <= N) and (0 < y <= M) and ((x,y) not in holes) and ((x,y) not in other_pices):
                         move_set.append((x,y))
 
             # get legal moves for knight piece keeping board bounds and not entering holes
             case "n":
                 moves = [(x - 1, y + 2), (x + 1, y + 2), (x + 2, y + 1), (x + 2, y - 1), (x + 1, y - 2), (x - 1, y - 2), (x - 2, y - 1), (x - 2, y+ 1)]
                 for x, y in moves:
-                    if (0 < x <= N) and (0 < y <= M) and (x,y) not in holes:
+                    if (0 < x <= N) and (0 < y <= M) and ((x,y) not in holes) and ((x,y) not in other_pices):
                         move_set.append((x,y))
             
             # get legal moves for rook piece keeping board bound and stopping upon approaching a hole
@@ -39,98 +42,94 @@ def my_solve(N, M, holes, pieces):
                 for i in range(1, max(N,M) + 1):
 
                     if NORTH:
-                        if (0 < y + i <= M) and (x, y + i) not in holes:
+                        if (0 < y + i <= M) and ((x, y + i) not in holes) and ((x, y + i) not in other_pices):
                             move_set.append((x, y + i))
                         else:
                             NORTH = False
-                            continue
                     if EAST:
-                        if (0 < x + i <= N) and (x + i, y) not in holes:
+                        if (0 < x + i <= N) and ((x + i, y) not in holes) and ((x + i, y) not in other_pices):
                             move_set.append((x + i, y))
                         else:
                             EAST = False
-                            continue
 
                     if SOUTH:
-                        if (0 < y - i <= M) and (x , y - i) not in holes:
+                        if (0 < y - i <= M) and ((x , y - i) not in holes) and ((x , y - i) not in other_pices):
                             move_set.append((x, y - i))
                         else:
                             SOUTH = False
-                            continue
 
                     if WEST:
-                        if (0 < x - i <= N) and (x - i, y) not in holes:
+                        if (0 < x - i <= N) and ((x - i, y) not in holes) and ((x - i, y) not in other_pices):
                             move_set.append((x - i, y))
                         else:
                             WEST = False
-                            continue
             
             # get legal moves for bishop piece keeping board bound and stopping upon approaching a hole
             case "b":
                 BR = BL = TR = TL = True
                 for i in range(1, max(N,M) + 1):
+                    if not BR and not BL and not TR and not TL: break
                     
                     # reaching for top right corner
                     if TR:
-                        if (0 < x + i <= N) and (0 < y + i <= M) and (x + i, y + i) not in holes:
+                        if (0 < x + i <= N) and (0 < y + i <= M) and ((x + i, y + i) not in holes) and ((x + i, y + i) not in other_pices):
                             move_set.append((x + i, y + i))
                         else:
                             TR = False
-                            continue
 
                     # reaching for top left corner
                     if TL:
-                        if (0 < x - i <= N) and (0 < y + i <= M) and (x - i, y + i) not in holes:
+                        if (0 < x - i <= N) and (0 < y + i <= M) and ((x - i, y + i) not in holes) and ((x - i, y + i) not in other_pices):
                             move_set.append((x - i, y + i))
                         else:
                             TL = False
-                            continue
                     
                     # reaching for bottom left corner
                     if BL:
-                        if (0 < x - i <= N) and (0 < y - i <= M) and (x - i, y - i) not in holes:
+                        if (0 < x - i <= N) and (0 < y - i <= M) and ((x - i, y - i) not in holes) and ((x - i, y - i) not in other_pices):
                             move_set.append((x - i, y - i))
                         else:
                             BL = False
-                            continue
                     
                     # reaching fot bottom right corner
                     if BR:
-                        if (0 < x + i <= N) and (0 < y - i <= M) and (x + i, y - i) not in holes:
+                        if (0 < x + i <= N) and (0 < y - i <= M) and ((x + i, y - i) not in holes) and ((x + i, y - i) not in other_pices):
                             move_set.append((x + i, y - i))
                         else:
                             BR = False
-                            continue
 
             # get legal moves for queen piece that combine all legal moves from rook and bishop moveset
-            case "q": move_set = get_move_set("r", x, y) + get_move_set("b", x, y)
+            case "q": move_set = get_move_set("r", x, y, other_pices) + get_move_set("b", x, y, other_pices)
         
         return move_set
     
-    def get_possible_boards(board : Board):
+    def get_possible_boards(board : Board) -> set[Board]:
         nonlocal id
         possible_boards = set()
+        other_pices = list(map(lambda elem: (elem[1], elem[2]), board.pieces_positions))
+
         for piece, x , y in board.pieces_positions:
-            move_set = get_move_set(piece, x, y)
+            move_set = get_move_set(piece, x, y, other_pices)
 
             for nx, ny in move_set:
-                new_pieces_positions = list(board.pieces_positions)
+                new_pieces_positions = set(board.pieces_positions)
                 new_pieces_positions.remove((piece, x, y))
-                new_pieces_positions.append((piece, nx, ny))
+                new_pieces_positions.add((piece, nx, ny))
                 possible_boards.add(Board(id, new_pieces_positions))
                 id += 1
         
         return possible_boards
     
+    holes = set(holes)
+    pieces = set(pieces)
+
     id = 0
     initial_board = Board(id, pieces)
     id += 1
-    visited_boards = set()
-    visited_boards.add(initial_board)
-
-    holes = set(holes)
 
     boards = [initial_board]
+    # boards_after_first_move = get_possible_boards(initial_board)
+    # boards_after_first_move = list(filter(lambda x: x not in boards, boards_after_first_move))
 
     result = False
 
@@ -151,11 +150,41 @@ def my_solve(N, M, holes, pieces):
             if new_board not in boards:
                 DFS_boards(boards + [new_board], not player)
 
-
-    # for i, j in holes:
-    #     pass
-
     DFS_boards(boards, True)
     return result
 
+    # def DFS_boards(boards : list[Board]):
+        
+    #     possible_boards = get_possible_boards(boards[-1])
+    #     possible_boards = list(filter(lambda x: x not in boards, possible_boards))
+
+    #     if not possible_boards:
+    #         boards[-1].is_winning = False
+    #         return
+        
+    #     for new_board in possible_boards:
+
+    #         if new_board not in boards:
+
+    #             DFS_boards(boards + [new_board])
+
+    #             boards[-1].is_winning = (not new_board.is_winning)
+
+
+    # # for i, j in holes:
+    # #     pass
+
+    # DFS_boards(boards)
+    # return initial_board.is_winning
+
+
 runtests(my_solve)
+
+# print(my_solve(2, 5,
+#   [(2, 1), (2, 3), (2, 4), (2, 5)],
+#   [("k", 2, 2)]))
+
+# print(my_solve(3, 3,
+#   [(2, 1), (2, 2), (2, 3)],
+#   [("k", 1, 2), ("q", 3, 3)],
+#   ))
